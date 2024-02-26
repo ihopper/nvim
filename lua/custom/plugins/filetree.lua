e810844f45c69551cb014a608cda76cb696e0bd6
Isaac Hopper <isaac.hopper@gmail.com>
2024-02-26 12:28:10 -0500

    update filetree config

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -2,14 +2,21 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function ()
-    require('neo-tree').setup {}
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = false,
+				},
+			},
+		})
+	end,
 }
